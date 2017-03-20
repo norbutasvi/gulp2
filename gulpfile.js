7ea731d1b5e180234a8331afce40922e22ed1f79
viliusnorbutas <norbutasvilius@gmail.com>
2017-03-20 19:10:26 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify-css');
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'web'
+    });
+});
+
+gulp.task('html', function() {
+    gulp.src('./web/*.html')
+});
+
+
+
+gulp.task('sass', function () {
+    return gulp.src('./src/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(minify())
+        .pipe(gulp.dest('./web/assets'));
+});
+
+gulp.task('js', function() {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./web/assets'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['./web/*.html'], ['html']);
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+    gulp.watch('./src/js/**/*.js', ['js']);
+});
+
+gulp.task('default', ['connect', 'sass', 'js', 'watch']);
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by USER on 3/17/2017.
+ */
